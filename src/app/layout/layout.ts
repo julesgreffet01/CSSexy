08f51c1274b3ef39f8475025bf9c0fd8cc201abb
Jules Greffet <julesgreffet@gmail.com>
2025-11-30 22:08:22 +0100

    jules test sa branche

---
+++ src/app/layout/layout.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {Header} from '../components/header/header';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    RouterOutlet,
+    Header
+  ],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css',
+})
+export class Layout {
+
+}
--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {Layout} from '../layout/layout';
+import {HomePage} from '../pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {path: 'home', component: HomePage},
+      ]
+  }
+];
