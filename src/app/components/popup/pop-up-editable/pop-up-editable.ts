9d5f1d1803b43baccf23f953cc422c2176184936
maxirrx <max2005.roux@gmail.com>
2025-12-01 16:38:26 +0100

    qsdf

--- src/app/components/popup/pop-up-editable/pop-up-editable.ts
+++ src/app/components/popup/pop-up-editable/pop-up-editable.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {Inputs} from '../../inputs/inputs';
+import {Buttons} from '../../buttons/buttons';
 
 @Component({
   selector: 'app-pop-up-editable',
-  imports: [],
+  imports: [
+    Inputs,
+    Buttons
+  ],
   templateUrl: './pop-up-editable.html',
   styleUrl: './pop-up-editable.css',
 })
 export class PopUpEditable {
 
-  title: string = "Ajout projet demain matin quand";
+  //type = input<'Projet' | 'Service'>();
+  //action = input<'Modification' | 'Ajout'>();
+  callback = input< ((arg?: string) => void)>()
+
+
+  type = 'Service';
+  action = 'Ajout';
+
+  public getType(){
+    return this.type;
+  }
+
+  public getAction(){
+    return this.action;
+  }
+
+  public getcallback(){
+    this.callback();
+  }
+
+  public getButtonName(){
+    if(this.action == 'Modification'){
+      return 'Modifier';
+    }else{
+      return 'Ajouter';
+    }
+  }
+
 
 }
