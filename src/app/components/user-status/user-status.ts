89904b8904673c60951150a57ee378f45263a878
Itikatchi <yann.neuville@outlook.fr>
2025-12-01 08:42:59 +0100

    [CSSexy]User status creation

---
+++ src/app/components/user-status/user-status.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-status',
+  imports: [],
+  templateUrl: './user-status.html',
+  styleUrl: './user-status.css',
+})
+export class UserStatus {
+    name: string = "Un prenom";
+    image = '/5907.jpg';
+}
