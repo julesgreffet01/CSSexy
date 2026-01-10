da31378af11f06e164f066354dd40033ead14c9a
Itikatchi <yann.neuville@outlook.fr>
2026-01-10 17:39:35 +0100

    [CSSexy-Yann] Service service and user

--- src/app/components/tabs/up-down-button/up-down-button.ts
+++ src/app/components/tabs/up-down-button/up-down-button.ts
@@ -1,11 +1,13 @@
-import { Component, input, output, signal } from '@angular/core';
+import { Component, inject, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-up-down-button',
   imports: [],
+  
   templateUrl: './up-down-button.html',
   styleUrl: './up-down-button.css',
 })
+
 export class UpDownButton {
   InitStat = input<boolean>(true);
   protected UpDownStat = signal<boolean>(true);
---
+++ src/app/core/services/service-services.ts
@@ -0,0 +1,56 @@
+import { inject, Injectable } from "@angular/core";
+import { ServiceModel } from "../../models/service-model";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+    providedIn: "root",
+})
+export class serviceServices {
+    private readonly http = inject(HttpClient);
+    private readonly baseUrl = "/services";
+
+    public getServices(service_uuid: string): Observable<ServiceModel[]> {
+        return this.http.get<ServiceModel[]>(`${this.baseUrl}/${service_uuid}`);
+    }
+
+    public updateService(service: ServiceModel): Observable<ServiceModel> {
+        return this.http.put<ServiceModel>(`${this.baseUrl}/${service.id}`, service);
+    }
+
+    public deleteService(service_uuid: string): Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}/${service_uuid}`);
+    }
+
+    public startService(service_uuid: string): Observable<void> {
+        return this.http.post<void>(`${this.baseUrl}/${service_uuid}/start`, {});
+    }
+
+    public stopService(service_uuid: string): Observable<void> {
+        return this.http.post<void>(`${this.baseUrl}/${service_uuid}/stop`, {});
+    }
+
+    public restartService(service_uuid: string): Observable<void> {
+        return this.http.post<void>(`${this.baseUrl}/${service_uuid}/restart`, {});
+    }   
+
+    public serviceMonitoring(service_uuid: string): Observable<any> {
+        return this.http.get<any>(`${this.baseUrl}/${service_uuid}/monitoring`);
+    }
+
+    public serviceMonitoringDetails(service_uuid: string, name: string): Observable<any> {
+        return this.http.get<any>(`${this.baseUrl}/${service_uuid}/monitoring/${name}`);
+    }
+    
+    public serviceDefMonitoring(service_uuid: string): Observable<any> {
+        return this.http.post<any>(`${this.baseUrl}/${service_uuid}/monitoring`, {});
+    }
+
+    public serviceMonitoringIndactors(service: ServiceModel, name: string): Observable<any> {
+        return this.http.put<any>(`${this.baseUrl}/${service.id}/monitoring/${name}`, service);
+    }
+
+    public serviceMonitoringMesures(service_uuid: string, name: string, datetime: Date): Observable<any> {
+        return this.http.get<any>(`${this.baseUrl}/${service_uuid}/monitoring/${name}/mesures/${datetime.toISOString()}`);
+    }
+}
